92d1a935f850a41a9b42b484fea28b9e9882c77d
Leo Caldosa <caldosaleo@gmail.com>
2025-06-22 19:15:19 +0800

    separated class and main py files

---
+++ main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from style_transfer_webcam import StyleTransferApp
+
+# Run the application
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = StyleTransferApp(root)
+    root.mainloop()
